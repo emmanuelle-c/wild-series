3ff55a488a465f9630c83c2cd5374fb6a6af66ab
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-05-21 12:33:24 +0200

    initialising route for programs

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -1,14 +1,20 @@
 const express = require("express");
 
 const router = express.Router();
+const { sayWelcome } = require("../../controllers/sayActions");
 
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
+router.get("/", sayWelcome);
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 /* ************************************************************************* */
 
